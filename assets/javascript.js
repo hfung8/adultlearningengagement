aa4a75842e77ecd1f48afa4431af48c6557cf8f8
Harrison Fung <harrison.hok.fung@gmail.com>
2021-02-16 20:45:45 -0500

    changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,6 +7,7 @@ var container = document.querySelector('.container');
 var page;
 var pageArray = [];
 var continueButton = document.querySelector(".continue");
+var warmUpButton;
 //helper functions
 var createElem = function(elem){
     var target = document.createElement(elem);
@@ -44,8 +45,6 @@ document.addEventListener('click', function (event) {
 	// Don't follow the link
     event.preventDefault();
 
-
-
 }, false);
 
 // starter code
@@ -54,6 +53,17 @@ startButton.addEventListener("click", function(event){
     setElements();
 })
 
+// (function(){
+//     var textArea = '<textarea></textarea>'
+//     warmUpButton = document.querySelector(".warmUpButton");
+//     warmUpButton.addEventListener('click', function(){
+//     console.log("hello");
+//     var warmup = document.querySelector(".warmup");
+//     document.querySelector(".warm").replaceChildNode(textArea, warmup);
+//     })
+// }())
+
+
 var page1 = '<div style="strong"><h2>The goal of this learning guide is to introduce you to our new Customer Engagement Model (CEM), so that across our organization we can achieve a common vision and approach for how we engage with and serve our customers.</h2></div>'
 page1 += '<h3>Learning Objectives:</h3>'
 page1 += '<h4>After completing the activities in this learning guide, all employees of First Premium Bank, will be able to:</h4>'
@@ -61,19 +71,23 @@ page1 += '<ul><li>Describe the phases of the new Customer Engagement Model.</li>
 
 var page2 = '<div style="strong"><h2>Reflection: What do Our Customers Value Today?</h2></div>'
 page2+= '<h3>According to a global survey conducted by The Marketing Consulting Group, a key driver, which is heralding change in our industry is Customer Value Evolution.</h3><br>'
-page2+= '<h3>Warm-Up Activity: Warm-Up Activity: Discuss the following with your colleagues:</h3>'
-page2+= '<ul><li>1. How have your customer’s expectations evolved over the past three years?</li><li>2. In your opinion, who do you consider delivers an exceptional customer experience? What makes their client experience memorable? (Note: could be any competitor, or even a service you seek outside of our industry i.e. a favourite coffee provider)</li><li>3. Consider our current customer experience. What do you think that we do well? What might we improve on?</li></ul>'
+page2+= '<div class="warmup"><div class="warm"><h3>Warm-Up Activity: Warm-Up Activity: Discuss the following with your colleagues:</h3>'
+page2+= '<ul><li>1. How have your customer’s expectations evolved over the past three years?</li><li>2. In your opinion, who do you consider delivers an exceptional customer experience? What makes their client experience memorable? (Note: could be any competitor, or even a service you seek outside of our industry i.e. a favourite coffee provider)</li><li>3. Consider our current customer experience. What do you think that we do well? What might we improve on?</li></ul></div></div>'
+page2+= '<textarea>Write down some of your thoughts</textarea>'
 
 var page3 = "<div style='background-image: url('./assets/businessGroup.jpg')'>What is a Customer Engagement Model?</div>"
-var array = [page1, page2, page3]
+var array = [page1, page2, page3];
+
+
 
 function createPage(){
     for ( var i = 0; i < array.length; i++){
         pageArray.push(array[i]);
     }
     page = pageArray.shift();
-    container.innerHTML = page;
-}
+    console.log(page);
+        container.innerHTML = page;
+    }
 
 function setElements(){
     addToStyle(customerEngagementDiv, `display:none`);
@@ -82,7 +96,9 @@ function setElements(){
         createPage();
     })
     createPage();
+
 }
+
 
 
 
