2ae5de84bbe01deab31c8ecbc74dc5119446379b
Harrison Fung <harrison.hok.fung@gmail.com>
2021-02-16 20:55:51 -0500

    changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -53,32 +53,21 @@ startButton.addEventListener("click", function(event){
     setElements();
 })
 
-// (function(){
-//     var textArea = '<textarea></textarea>'
-//     warmUpButton = document.querySelector(".warmUpButton");
-//     warmUpButton.addEventListener('click', function(){
-//     console.log("hello");
-//     var warmup = document.querySelector(".warmup");
-//     document.querySelector(".warm").replaceChildNode(textArea, warmup);
-//     })
-// }())
-
-
 var page1 = '<div style="strong"><h2>The goal of this learning guide is to introduce you to our new Customer Engagement Model (CEM), so that across our organization we can achieve a common vision and approach for how we engage with and serve our customers.</h2></div>'
 page1 += '<h3>Learning Objectives:</h3>'
 page1 += '<h4>After completing the activities in this learning guide, all employees of First Premium Bank, will be able to:</h4>'
 page1 += '<ul><li>Describe the phases of the new Customer Engagement Model.</li><li>Consider our customer’s needs at each phase.</li><li>Identify the key behaviours that employees should be able to demonstrate in response to customer needs at each phase.</li><li>Reflect on the current customer service that you are delivering and on how it can be improved on using the new model.</li></ul>'
 
-var page2 = '<div style="strong"><h2>Reflection: What do Our Customers Value Today?</h2></div>'
-page2+= '<h3>According to a global survey conducted by The Marketing Consulting Group, a key driver, which is heralding change in our industry is Customer Value Evolution.</h3><br>'
+var page2 = '<div style="strong"><h3>Reflection: What do Our Customers Value Today?</h3></div>'
+page2+= `<h4>According to a global survey conducted by The Marketing Consulting Group, a key driver, which is heralding change in our industry is Customer Value Evolution.<h4 style="color:white; font-style:italic">Today's customers are cautious, smart, less trusting and loyal and now demanding better service and clearer value
+from their business relationships. They are constantly asking, "Do you really provide a value add for me?"</h4>
+</h4><br>`
 page2+= '<div class="warmup"><div class="warm"><h3>Warm-Up Activity: Warm-Up Activity: Discuss the following with your colleagues:</h3>'
 page2+= '<ul><li>1. How have your customer’s expectations evolved over the past three years?</li><li>2. In your opinion, who do you consider delivers an exceptional customer experience? What makes their client experience memorable? (Note: could be any competitor, or even a service you seek outside of our industry i.e. a favourite coffee provider)</li><li>3. Consider our current customer experience. What do you think that we do well? What might we improve on?</li></ul></div></div>'
 page2+= '<textarea>Write down some of your thoughts</textarea>'
 
 var page3 = "<div style='background-image: url('./assets/businessGroup.jpg')'>What is a Customer Engagement Model?</div>"
 var array = [page1, page2, page3];
-
-
 
 function createPage(){
     for ( var i = 0; i < array.length; i++){
